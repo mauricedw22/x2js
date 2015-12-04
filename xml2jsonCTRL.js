5e90ab0b4bb851e03657808a0361c1deb1470ca9
Maurice Walker <mauricedw22@gmail.com>
2015-12-04 13:49:08 -0500

    xml2jsonCTRL.js
    CTRL converting xml to json

---
+++ xml2jsonCTRL.js
@@ -0,0 +1,17 @@
+
+var app = angular.module('app', []);  
+app.controller('appCTRL', function($scope, $http)  
+{  
+    $http.get("Sitemap.xml",  
+    {  
+        transformResponse: function(cnv)  
+        {  
+            var x2js = new X2JS();  
+            var aftCnv = x2js.xml_str2json(cnv);  
+            return aftCnv;  
+        }  
+    }).success(function(response)  
+    {  
+        console.log(response);  
+    });  
+});  
